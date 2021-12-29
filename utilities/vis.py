b005e031cbe2de730abaeb14bc9b239e1b73353c
Gökhan Yalnız <gokhan.yalniz@ist.ac.at>
2021-12-29 18:28:56 +0300

    finished with the pyvista script for channelflow. there is no need to interpolate to a uniform grid, but i have that available as an option.

--- utilities/vis.py
+++ utilities/vis.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import sys
 from pathlib import Path
 
 import h5py
@@ -7,6 +8,11 @@ import numpy as np
 import pyvista as pv
 import scipy.interpolate as spi
 
+# Set the paths to neubauten and channelflow below
+# fmt: off
+# This adds channelflow/build/python-wrapper to the interpreter's path
+channelflowPython = Path('/nfs/scistore12/hofgrp/gyalniz/') / 'channelflow/build-gcc10.2/python-wrapper'
+sys.path.append(str(channelflowPython.resolve()))
 import libpycf as cf
 
 
@@ -23,14 +29,14 @@ def main():
         "--xvfb", action="store_true", dest="xvfb", help="render to a virtual display."
     )
     parser.add_argument(
-        "--addbase",
+        "--mirror_y",
         action="store_true",
-        dest="addbase",
-        help="remove laminar part from states.",
+        dest="mirror_y",
+        help="display the fundamental domain of mirror_y.",
     )
     parser.add_argument(
         "-cvel",
-        default=0.75,
+        default=0.5,
         type=float,
         dest="cvel",
         help="multiplier for velocity isosurfaces",
@@ -49,7 +55,7 @@ def main():
 
 
 def dnsvis(
-    state, noshow=False, xvfb=False, addbase=False, cvel=0.75, cvor=0.5,
+    state, noshow=False, xvfb=False, mirror_y=False, cvel=0.75, cvor=0.5,
 ):
 
     if xvfb:
@@ -59,13 +65,6 @@ def dnsvis(
     pv.set_plot_theme("document")
     state = Path(state)
     velocity = cf.FlowField(str(state.resolve()))
-    nx_ = velocity.Nx
-    ny_ = velocity.Ny
-    nz_ = velocity.Nz
-    lx = velocity.Lx
-    lz = velocity.Lz
-    a = velocity.a
-    b = velocity.b
 
     vorticity = cf.curl(velocity)
     vorticity.zeroPaddedModes()
@@ -74,39 +73,55 @@ def dnsvis(
 
     xgrid, ygrid, zgrid, velx, _, _ = channelflow_to_numpy(state)
     _, _, _, vorx, _, _ = channelflow_to_numpy(state_vorticity)
-    dx, dz = xgrid[1] - xgrid[0], zgrid[1] - zgrid[0]
     nx, ny, nz = len(xgrid), len(ygrid), len(zgrid)
-    # print(f"lx, ly, lz = {dx*nx}, {ygrid[-1]-ygrid[0]}, {dz*nz}")
-    # print(f"nx, ny, nz = {nx}, {ny}, {nz}")
+    lx = velocity.Lx
+    lz = velocity.Lz
+    print(f"nx, ny, nz = {nx}, {ny}, {nz}")
+    print(f"lx, lz = {lx}, {lz}")
 
-    u = pv.wrap(velx)
-    om = pv.wrap(vorx)
+    ny_display = ny
+    if mirror_y:
+        if ny % 2 != 0:
+            ny_display = ny // 2 + 1
+        else:
+            ny_display = ny // 2
 
-    vel_levels = cvel * np.array([np.min(velx), np.max(velx)])
-    vor_levels = cvor * np.array([np.min(vorx), np.max(vorx)])
+        ygrid = ygrid[-ny_display:]
+        velx = velx[:, -ny_display:, :]
+        vorx = vorx[:, -ny_display:, :]
+        print("ny_display = ", ny_display)
+    else:
+        ny_display = ny
+
+    vel_levels = cvel * np.array([np.amin(velx), np.amax(velx)])
+    vor_levels = cvor * np.array([np.amin(vorx), np.amax(vorx)])
+
+    grid = pv.RectilinearGrid(xgrid, ygrid, zgrid)
+    # sad to need order="F" here
+    grid.point_data["velx"] = np.reshape(velx, (nx * ny_display * nz), order="F")
+    grid.point_data["vorx"] = np.reshape(vorx, (nx * ny_display * nz), order="F")
 
     p = pv.Plotter(off_screen=noshow)
-
-    p.add_mesh(u.outline(), color="k")
+    p.add_mesh(grid.outline(), color="k")
     p.add_mesh(
-        u.contour(vel_levels),
+        grid.contour(isosurfaces=vel_levels, scalars="velx"),
         smooth_shading=True,
         opacity=0.35,
-        cmap=["red", "blue"],
+        cmap=["blue", "red"],
         clim=vel_levels,
         show_scalar_bar=False,
     )
     p.add_mesh(
-        om.contour(vor_levels),
+        grid.contour(isosurfaces=vor_levels, scalars="vorx"),
         smooth_shading=True,
         opacity=0.35,
-        cmap=["green", "purple"],
+        cmap=["purple", "green"],
         clim=vor_levels,
         show_scalar_bar=False,
     )
     p.show_axes()
+    p.show_bounds(xlabel="x", ylabel="y", zlabel="z")
 
-    #
     p.camera.roll += 90
     p.camera.elevation -= 15
     p.camera.azimuth -= 45
@@ -114,10 +129,16 @@ def dnsvis(
     p.camera.azimuth -= 45
     p.camera.roll -= 10
 
+    # p.camera.position = [-3.8165567986481825, 2.432563985013201, 1.623442321119651]
+    # p.camera.focal_point = [1.5630592574663666, 0.15588548135152808, 0.4599843715554596]
+    # p.camera.view_angle = 30.0
+    # p.camera.view_up = [0.37308813472900676, 0.9240636797097904, -0.08313578992006013]
+    # p.camera.clipping_range = [2.2454569218403426, 10.160445581565819]
+
     p.show(screenshot=f"{state.stem}_isosurf.png")
 
 
-def channelflow_to_numpy(statefile, uniform=True):
+def channelflow_to_numpy(statefile, uniform=False):
 
     with h5py.File(statefile, mode="r") as file:
 
